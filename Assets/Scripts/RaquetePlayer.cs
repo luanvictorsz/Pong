883ec92e9f4c45aa96091a9b5ef36110818fc57c
Luan Victor (A3TERNUS) <luanvitorlima2017@gmail.com>
2024-12-21 13:11:00 -0300

    Movimentos da Raquete
    Movimentando a Primeira Raquete e limitando a posição para que ela não saia da cena

--- Assets/Scripts/RaquetePlayer.cs
+++ Assets/Scripts/RaquetePlayer.cs
@@ -2,15 +2,33 @@ using UnityEngine;
 
 public class RaquetePlayer : MonoBehaviour
 {
+    private Vector3 myPos;
+    private float posY;
+    [SerializeField] private float limit;
+    [SerializeField] private float speed;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        myPos = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        myPos.y = posY; 
+        transform.position = myPos;
+
+        if(Input.GetKey(KeyCode.UpArrow))
+        {       
+
+            if(posY < limit)
+            {
+                posY += speed * Time.deltaTime;
+            }
+        }
+        if(Input.GetKey(KeyCode.DownArrow) && posY > -limit)
+        {
+            posY -= speed * Time.deltaTime;
+        }
     }
 }
