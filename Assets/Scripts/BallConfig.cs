70edc64368d941e6755fe49b541899fd7d53be4c
Luan Victor (A3TERNUS) <luanvitorlima2017@gmail.com>
2024-12-22 12:16:10 -0300

    Mecanica
    Adicionando o movimento da Bolinha e Colisão com as paredes possibilitando quicar dentre a area

--- Assets/Scripts/BallConfig.cs
+++ Assets/Scripts/BallConfig.cs
@@ -2,10 +2,14 @@ using UnityEngine;
 
 public class BallConfig : MonoBehaviour
 {
+    public Rigidbody2D rig;
+    public float speed;
+    public Vector2 ballSpeed;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        ballSpeed.x = speed;
+        rig.linearVelocity = ballSpeed;
     }
 
     // Update is called once per frame
